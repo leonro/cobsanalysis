757ca2bf0e653319585b87d7889cf59c53506a30
Sagittarius <r.leonhardt@zamg.ac.at>
2020-05-29 14:47:33 +0000

    corrected except clause in flagging

--- DataProducts/flagging.py
+++ DataProducts/flagging.py
@@ -338,8 +338,8 @@ if step1:
                        flaglist = consecutive_check(flagls, remove=True)
                        if len(flagls) > len(flaglist)+1:   #+1 to add some room
                            statusmsg[name2] = 'Step1: consecutive flags (unlikely related to spikes) were removed: Found {}, Clean: {}'.format(len(flagls), len(flaglist))
-                    except:
-                        pass
+                       #except:
+                       #pass
                        #flaglist = flagls
                        print ("  - new flags: {}".format(len(flagls)))
                     if lowlimit:
