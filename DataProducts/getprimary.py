7ab7bacd7e5184bf6bf0f73a68ef4e0df7dcbb00
leonro <roman.leonhardt@zamg.ac.at>
2020-12-10 18:39:55 +0100

    updated getprimary and removed some old scripts

--- DataProducts/getprimary.py
+++ DataProducts/getprimary.py
@@ -11,49 +11,19 @@ import json
 # old
 from pickle import dump
 
-
-## New Logging features 
+coredir = os.path.abspath(os.path.join('/home/cobs/MARTAS', 'core'))
+sys.path.insert(0, coredir)
 from martas import martaslog as ml
-logpath = '/var/log/magpy/mm-dp-getprimary.log'
-#import socket
-#sn = socket.gethostname().upper()
-sn = 'SAGITTARIUS' # servername ### Get that automatically??
-statusmsg = {}
-name1 = "{}-DataProducts-getprimary-process".format(sn)
-name2 = "{}-DataProducts-getprimary-vario".format(sn)
-name3 = "{}-DataProducts-getprimary-scalar".format(sn)
-name4 = "{}-DataProducts-getprimary-write".format(sn)
+from acquisitionsupport import GetConf2 as GetConf
 
 
-dbpasswd = mpcred.lc('cobsdb','passwd')
-try:
-    # Test MARCOS 1
-    print ("Connecting to primary MARCOS...")
-    db = mysql.connect(host="138.22.188.195",user="cobs",passwd=dbpasswd,db="cobsdb")
-except:
-    print ("... failed")
-    try:
-        # Test MARCOS 2
-        print ("Connecting to secondary MARCOS...")
-        db = mysql.connect(host="138.22.188.191",user="cobs",passwd=dbpasswd,db="cobsdb")
-    except:
-        print ("... failed -- aborting")
-        sys.exit()
+"""
+Methods in several scripts:
+ConnectDatabases()
+DefineLogger()
 
-endtime = datetime.utcnow()
-daystodeal = 1
-starttime=datetime.strftime(endtime-timedelta(days=daystodeal),"%Y-%m-%d")
-dbdateformat = "%Y-%m-%d %H:%M:%S.%f"
+"""
 
-#Instrument Lists
-variolist = ['LEMI036_1_0002_0002','LEMI025_22_0003_0002','FGE_S0252_0001_0001']
-scalalist = ['GP20S3NSS2_012201_0001_0001','GSM90_14245_0002_0002','GSM90_6107631_0001_0001','POS1_N432_0001_0001']
-
-currentvaluepath = '/srv/products/data/current.data'
-varioinst = ''
-scalainst = ''
-#old
-priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
 
 def getstringdate(input):
     dbdateformat1 = "%Y-%m-%d %H:%M:%S.%f"
@@ -68,35 +38,115 @@ def getstringdate(input):
             return datetime.utcnow()
     return value
 
-#part1 = True
-try:
-    #if part1:
+def DefineLogger(config={}, category="DataProducts", newname=''):
+    host = socket.gethostname()
+    jobname = os.path.splitext(os.path.basename(__file__))[0]
+    name = "{}-{}-{}".format(host.upper(),category,jobname)
+    # extract loggingpath from config
+    if not newname == '':
+        logpath = os.path.abspath(config.get(logfile))
+        logdir = logpath.splittext[0]
+        logpath = ps.path.join(logdir,newname)
+        config['logfile'] = logpath
+    # add name to config dict
+    config['logname'] = name
+
+    return config
+
+
+def ConnectDatabases(config={}, debug=False):
+    """
+    DESCRIPTION:
+        Database connection
+    """
+
+    connectdict = {}
+    config['primaryDB'] = None
+
+    dbcreds = config.get('dbcredentials')
+    if not isinstance(dbcreds,list):
+        dbcreds = [dbcreds]
+
+    # First in list is primary
+    for credel in dbcreds:
+        # Get credentials
+        dbpwd = mpcred.lc(credel,'passwd')
+        dbhost = mpcred.lc(credel,'host')
+        dbuser = mpcred.lc(credel,'user')
+        dbname = mpcred.lc(credel,'db')
+
+        # Connect DB
+        if dbhost:
+            try:
+                if debug:
+                    print ("    -- Connecting to database {} ...".format(credel))
+                    print ("    -- {} {} {}".format(dbhost, dbuser, dbname))
+                connectdict[credel] = mysql.connect(host=dbhost,user=dbuser,passwd=dbpwd,db=dbname)
+                if debug:
+                    print ("...success")
+            except:
+                pass
+
+    if len(connectdict) == 0:
+        print ("  No database found - aborting")
+        sys.exit()
+    else:
+        if debug:
+            print ("    -- at least on db could be connected")
+
+    if connectdict.get(dbcreds[0],None):
+        if debug:
+            print ("    -- primary db is available: {}".format(dbcreds[0]))
+        config['primaryDB'] = connectdict[dbcreds[0]]
+
+    config['conncetedDB'] = connectdict
+
+    return config
+
+
+def PrimaryVario(db, variolist, endtime=datetime.utcnow(), logname='', statusmsg={}, debug=False):
     """
     Identify currently active variometer and f-instrument, which are recording now and have at least one day of data
     """
     ## Step 1: checking available data for variometers (need to cover one day and should not be older than one hour 
     ##         the first instrument fitting these conditions is selected
     for inst in variolist:
-        print ("Checking ", inst)
+        if debug:
+            print ("    -- Checking ", inst)
         last = dbselect(db,'time',inst,expert="ORDER BY time DESC LIMIT 86400")
-        print (len(last), last[0], last[-1])
         if len(last) > 0:
             # convert last to datetime
-            lastval = getstringdate(last[0]) #datetime.strptime(last[0],dbdateformat)
-            firstval = getstringdate(last[-1]) #datetime.strptime(last[-1],dbdateformat)
+            lastval = getstringdate(last[0])
+            firstval = getstringdate(last[-1])
             print (lastval,firstval)
             if lastval > endtime-timedelta(minutes=60) and firstval > endtime-timedelta(minutes=1500):
                 varioinst = inst
-                print ("Coverage OK - using {}".format(inst))
+                if debug:
+                    print ("    -- Coverage OK - using {}".format(inst))
                 break
             else:
-                print ("Coverage not OK")
-    statusmsg[name2] = '{}'.format(varioinst.replace('_','-'))
+                print ("    -- Coverage not OK")
+        print ("  -> Selected: {}".format(varioinst))
 
+    if varioinst == '':
+        print (" -- Did not find variometer instrument")
+        varioinst = variolist[0]
+        print ("  -> Selected fallback: {}".format(varioinst)) 
+        statusmsg[logname] = 'No recent variometer data found - switching to fallback - will automatically continue without further notice until DB/collector are online again'
+    else:
+        statusmsg[logname] = '{}'.format(varioinst.replace('_','-'))
+
+    return varioinst, statusmsg
+
+
+def PrimaryScalar(db, scalarlist, endtime=datetime.utcnow(), logname='', statusmsg={}, debug=False):
+    """
+    Identify currently active variometer and f-instrument, which are recording now and have at least one day of data
+    """
     ## Step 2: checking available scalar data (need to be valid data and cover one day)
     ##         the first instrument fitting these conditions is selected
     for inst in scalalist:
-        print ("Checking ", inst)
+        print ("    -- Checking ", inst)
         try:
             sr = dbselect(db,'DataSamplingRate','DATAINFO','DataID LIKE "{}"'.format(inst))
             sr = float(sr[0])
@@ -111,50 +161,35 @@ try:
         print (len(last), getstringdate(last[0]))
         if len(last) > 0 and lval and not lval[0] in [0,np.nan]:
             # convert last to datetime
-            print ("Test 1 OK: data  available and last value not null")
-            lastval = getstringdate(last[0]) # datetime.strptime(last[0],dbdateformat)
-            firstval = getstringdate(last[-1]) # datetime.strptime(last[-1],dbdateformat)
-            print (lastval, firstval)
+            if debug:
+                print ("    -- Test 1 OK: data  available and last value not null")
+            lastval = getstringdate(last[0])
+            firstval = getstringdate(last[-1])
+            if debug:
+                print (lastval, firstval)
             if lastval > endtime-timedelta(minutes=60) and firstval > endtime-timedelta(minutes=1500):
-                print ("Test 2 OK: correct amount of data covering the last day")
+                if debug:
+                    print ("    -- Test 2 OK: correct amount of data covering the last day")
                 scalainst = inst
                 break
             else:
-                print ("Test 2 failed: {} is smaller than {} or {} is smaller than {}".format(lastval,endtime-timedelta(minutes=60),firstval,endtime-timedelta(minutes=1500)))
+                if debug:
+                    print ("    -- Test 2 failed: {} is smaller than {} or {} is smaller than {}".format(lastval,endtime-timedelta(minutes=60),firstval,endtime-timedelta(minutes=1500)))
+        print ("  -> Selected: {}".format(scalainst))
 
-    statusmsg[name3] = '{}'.format(scalainst.replace('_','-'))
-    # Fallback
-    if varioinst == '':
-        print (" -- Did not find variometer instrument")
-        varioinst = variolist[0]
     if scalainst == '':
-        print (" -- Did not find scalar instrument")
+        print ("  !! Did not find scalar instrument")
         scalainst = scalalist[0]
-        statusmsg[name3] = 'No recent scalar data found - check collector jobs and DB - switching to default - will automatically continue without further notice as soon as DB or collector is online again'
+        print ("  -> Selected fallback: {}".format(scalainst)) 
+        statusmsg[logname] = 'No recent scalar data found - switching to fallback - will automatically continue without further notice until DB/collector are online again'
+    else:
+        statusmsg[logname] = '{}'.format(scalainst.replace('_','-'))
 
-    print ("Get primary -> Using Variometer {} and Scalar {}".format(varioinst, scalainst))
+    return scalainst, statusmsg
 
+
+def UpdateCurrentValuePath(currentvaluepath, varioinst='', scalainst='', logname='', statusmsg={}, debug=False)
     try:
-        print ("Dumping instruments as usual in the old way...")
-        #old
-        """
-        /home/cobs/ANALYSIS/TitleGraphs/mag_graph.py:62:    priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        /home/cobs/ANALYSIS/DataProducts/GIC/src/gic_dailyplot.py:58:        priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        /home/cobs/ANALYSIS/DataProducts/StormDetection/StormDetector_brain.py:860:    priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        /home/cobs/ANALYSIS/DataProducts/StormDetection/StormDetector_graph.py:216:        priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        /home/cobs/ANALYSIS/DataProducts/magnetism_products.py:228:    #priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        /home/cobs/ANALYSIS/Info/tg_base.py:43:priminst = '/home/cobs/ANALYSIS/Logs/primaryinst.pkl'
-        """
-        lst = [varioinst, scalainst]
-        dump(lst,open(priminst,'wb'))
-        print ("... done")
-    except:
-        pass
-
-    try:
-        #ok = True
-        #if ok:
-        # writing data to current.data json 
         if os.path.isfile(currentvaluepath):
             # read log if exists and exentually update changed information
             # return changes
@@ -164,22 +199,121 @@ try:
         else:
             valdict = {}
             fulldict = {}
-            #fulldict['meteo'] = valdict
-        print ("Got", fulldict)
+        print ("    -- Got", valdict)
         valdict[u'primary vario'] = [varioinst,'']
         valdict[u'primary scalar'] = [scalainst,'']
 
         fulldict[u'magnetism'] = valdict
         with open(currentvaluepath, 'w',encoding="utf-8") as file:
             file.write(unicode(json.dumps(fulldict))) # use `json.loads` to do $
-        statusmsg[name4] = 'values written'
+        statusmsg[logname] = 'values written'
     except:
-        statusmsg[name4] = 'problem when writing current values '
-    statusmsg[name1] = 'successful'
-except:
-    statusmsg[name1] = 'failed'
+        statusmsg[logname] = 'problem when writing current values '
 
-martaslog = ml(logfile=logpath,receiver='telegram')
-martaslog.telegram['config'] = '/home/cobs/SCRIPTS/telegram_notify.conf'
-martaslog.msg(statusmsg)
+    return statusmsg
+
+
+def main(argv):
+    version = '1.0.0'
+    configpath = ''
+    statusmsg = {}
+    debug=False
+    endtime = datetime.utcnow()
+    varioinst = ''
+    scalainst = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"hc:e:D",["config=","endtime=","debug=",])
+    except getopt.GetoptError:
+        print ('getprimary.py -c <config>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('-------------------------------------')
+            print ('Description:')
+            print ('-- getprimary.py will determine the primary instruments --')
+            print ('-----------------------------------------------------------------')
+            print ('detailed description ..')
+            print ('...')
+            print ('...')
+            print ('-------------------------------------')
+            print ('Usage:')
+            print ('python getprimary.py -c <config>')
+            print ('-------------------------------------')
+            print ('Options:')
+            print ('-c (required) : configuration data path')
+            print ('-------------------------------------')
+            print ('Application:')
+            print ('python getprimary.py -c /etc/marcos/analysis.cfg')
+            sys.exit()
+        elif opt in ("-c", "--config"):
+            # delete any / at the end of the string
+            configpath = os.path.abspath(arg)
+        elif opt in ("-e", "--endtime"):
+            # get an endtime
+            endtime = arg.split(',')
+        elif opt in ("-D", "--debug"):
+            # delete any / at the end of the string
+            debug = True
+
+    print ("Running getprimary version {}".format(version))
+    print ("--------------------------------")
+
+    if not os.path.exists(configpath):
+        print ('Specify a valid path to configuration information')
+        print ('-- check magnetism_products.py -h for more options and requirements')
+        sys.exit()
+
+    print ("1. Read and check validity of configuration data")
+    config = GetConf(configpath)
+
+    print ("2. Activate logging scheme as selected in config")
+    config = DefineLogger(config=config, category = "DataProducts", newname='mm-dp-getprimary.log')
+
+    name1 = "{}-process".format(config.get('logname'))
+    name2 = "{}-vario".format(config.get('logname'))
+    name3 = "{}-scalar".format(config.get('logname'))
+    name4 = "{}-write".format(config.get('logname'))
+    statusmsg[name1] = 'successful'
+
+    print ("3. Connect databases and select first available")
+    try:
+        config = ConnectDatabases(config=config, debug=debug)
+        db = config.get('conncetedDB')[0]
+    except:
+        statusmsg[name1] = 'database failed'
+
+    print ("4. Checking variometer instruments")
+    try:
+        varioinst, statusmsg = PrimaryVario(db, config.get('variometerinstruments'), endtime=endtime,  logname=name2, statusmsg=statusmsg, debug=debug)
+        print ("   -> Using {}".format(varioinst))
+    except:
+        statusmsg[name1] = 'vario failed'
+
+    print ("5. Checking scalar instruments")
+    try:
+        scalainst, statusmsg = PrimaryScalar(db, config.get('variometerinstruments'), endtime=endtime,  logname=name3, statusmsg=statusmsg, debug=debug)
+        print ("   -> Using {}".format(scalainst))
+    except:
+        statusmsg[name1] = 'scalar failed'
+
+    print ("6. Updating Current values")
+    try:
+        statusmsg = UpdateCurrentValuePath(config.get('currentvaluepath'), varioinst=varioinst, scalainst=scalainst, logname=name4, statusmsg=statusmsg, debug=debug)
+    except:
+        statusmsg[name1] = 'current value update failed'
+
+
+    if not debug:
+        #martaslog = ml(logfile=logpath,receiver='telegram')
+        #martaslog.telegram['config'] = '/home/cobs/SCRIPTS/telegram_notify.conf'
+        #martaslog.msg(statusmsg)
+        pass
+    else:
+        print ("Debug selected - statusmsg looks like:")
+        print (statusmsg)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
 
