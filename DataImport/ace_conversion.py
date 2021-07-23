a2301c1f272e3a0e1bd019a89078176e996d27b3
leonro <roman.leonhardt@zamg.ac.at>
2021-07-23 11:39:11 +0200

    transferred files

---
+++ DataImport/ace_conversion.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+#********************************************************************
+# A script to continuously download ACE data during the day to keep 
+# the files up-to-date.
+# Currently running every 15 mins - :07,:22,:37,:52.
+#
+# Created 2015-02-17 by RLB. Updated 2020-12-23 by RL.
+# Adapted from /home/leon/CronScripts/saturn_wikactivity.py
+# Activated as cronjob on saturn 2015-03-02.
+#
+# rewritten by leon (2021-05-07)
+#********************************************************************
+
+from magpy.stream import read
+from magpy.database import *
+import magpy.opt.cred as mpcred
+#from pyproj import Geod
+import getopt
+import pwd
+import socket
+
+
+coredir = os.path.abspath(os.path.join('/home/cobs/MARTAS', 'core'))
+sys.path.insert(0, coredir)
+from martas import martaslog as ml
+from acquisitionsupport import GetConf2 as GetConf
+scriptpath = os.path.dirname(os.path.realpath(__file__))
+anacoredir = os.path.abspath(os.path.join(scriptpath, '..', 'core'))
+sys.path.insert(0, anacoredir)
+from analysismethods import DefineLogger, ConnectDatabases, getstringdate
+
+def merge_ACE(streama, streamb, keys):
+    # Merge streamb into streama without interpolation
+
+    for key in keys:
+        a = streamb._get_column(key)
+        streama._put_column(a, key)
+        streama.header['col-'+key] = streamb.header['col-'+key]
+        streama.header['unit-col-'+key] = streamb.header['unit-col-'+key]
+
+    return streama
+
+
+def process_ACE(datum, ace_P, ace_types, merge_variables, skipcompression=False, localpath='', debug=False):
+    """
+    Processes new data and adds it to old stream.
+
+    INPUT:
+    ace_P:             (str) String describing data type: '1m' or '5m'
+    ace_types   :       (list) List of type for type, e.g. ["swepam" and "mag"]
+    marge_variables:    (list) Variables to merge from type 2 into 1, e.g.:
+                        ['x','y','z','f','t1','t2']
+    """
+
+    print("Processing {} data for {} ...".format(ace_P,datum))
+    newday = False
+
+    # Read current data
+    if debug:
+        print (" Accessing {}".format(os.path.join(localpath,'raw','{}_ace_{}_{}.txt'.format(datum, ace_types[0], ace_P))))
+    ace_stream1 = read(os.path.join(localpath,'raw','{}_ace_{}_{}.txt'.format(datum, ace_types[0], ace_P)))
+    ace_stream2 = read(os.path.join(localpath,'raw','{}_ace_{}_{}.txt'.format(datum, ace_types[1], ace_P)))
+    if not ace_stream1.length()[0] > 0 and not ace_stream2.length()[0] > 0:
+        print (" No ACE data found - aborting")
+    else:
+        print ("{}: got {} datapoints".format(ace_types[0],ace_stream1.length()[0]))
+        print ("{}: got {} datapoints".format(ace_types[1],ace_stream2.length()[0]))
+
+    lastval = num2date(ace_stream1.ndarray[0][-1])
+    today = datetime.strftime(lastval, "%Y-%m-%d")
+    yesterday = datetime.strftime(lastval-timedelta(days=1), "%Y-%m-%d")
+
+    ace_file = os.path.join(localpath,'collected','ace_%s_%s.cdf' % (ace_P, today))
+    ace_lastfile = os.path.join(localpath,'collected','ace_%s_%s.cdf' % (ace_P, yesterday))
+
+    lastfile = True
+    if os.path.exists(ace_file):
+        try:
+            ace_last = read(ace_file)
+        except:
+            lastfile = False
+    else:
+        try:
+            ace_last = read(ace_lastfile)
+            newday = True
+        except:
+            lastfile = False
+
+    if lastfile:
+        if len(ace_last.ndarray[KEYLIST.index("var1")]) == 0:
+            lastfile = False
+
+    # Merging streams wrt time with no interpolation:
+    #ace_data = merge_ACE(ace_stream1, ace_stream2, merge_variables)
+    ace_data = mergeStreams(ace_stream1, ace_stream2, keys=merge_variables)
+
+
+    print (ace_data.length()[0])
+
+    if lastfile:
+         append == True
+         for key in merge_variables+['var1','var2','var3']:
+             keyind = KEYLIST.index(key)
+             if len(ace_last.ndarray[keyind]) == 0:
+                 if debug:
+                     print (keyind, len(ace_last.ndarray[keyind]))
+                 print("Error in data - not appending.")
+                 append == False
+         if append:
+             ace_data = appendStreams([ace_last, ace_data])
+
+    print (ace_data.length())
+
+    if not debug:
+      if newday == True:
+        print ("Created new {} file. Writing last data to yesterday.".format(ace_P))
+        ace_data.write(os.path.join(localpath,'collected'),filenamebegins="ace_%s_" % ace_P,
+                format_type='PYCDF', skipcompression=skipcompression)
+      else:
+        ace_data = ace_data.trim(starttime=today+"T00:00:00")
+        ace_data.write(os.path.join(localpath,'collected'),filenamebegins="ace_%s_" % ace_P,
+                format_type='PYCDF', skipcompression=skipcompression) # XXX
+    return True
+
+def main(argv):
+    version = '1.0.0'
+    configpath = ''
+    statusmsg = {}
+    debug=False
+    stime = None
+    etime = datetime.utcnow()
+    endtime = ''
+    starttime = ''
+    path = '/srv/archive/external/esa-nasa/ace'
+
+    try:
+        opts, args = getopt.getopt(argv,"hc:e:j:p:s:o:D",["config=","endtime=","joblist=","debug="])
+    except getopt.GetoptError:
+        print ('ace_conversion.py -c <config>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('-------------------------------------')
+            print ('Description:')
+            print ('-- ace_conversion.py will determine the primary instruments --')
+            print ('-----------------------------------------------------------------')
+            print ('detailed description ..')
+            print ('...')
+            print ('...')
+            print ('-------------------------------------')
+            print ('Usage:')
+            print ('python ace_conversion.py -c <config>')
+            print ('-------------------------------------')
+            print ('Options:')
+            print ('-c (required) : configuration data path')
+            print ('-e            : endtime, default is now')
+            print ('-p            : path for neic data')
+            print ('-------------------------------------')
+            print ('Application:')
+            print ('python neic_download.py -c /etc/marcos/analysis.cfg -p /home/cobs/ANALYSIS/Seismo/neic_quakes.d')
+            sys.exit()
+        elif opt in ("-c", "--config"):
+            # delete any / at the end of the string
+            configpath = os.path.abspath(arg)
+        elif opt in ("-e", "--endtime"):
+            # get an endtime
+            endtime = arg.split(',')
+        elif opt in ("-s", "--starttime"):
+            # get a starttime
+            starttime = arg.split(',')
+        elif opt in ("-p", "--path"):
+            path = arg
+        elif opt in ("-D", "--debug"):
+            # delete any / at the end of the string
+            debug = True
+
+    print ("Running flagging version {}".format(version))
+    print ("--------------------------------")
+
+    if not os.path.exists(configpath):
+        print ('Specify a valid path to configuration information')
+        print ('-- check magnetism_products.py -h for more options and requirements')
+        sys.exit()
+
+    print ("1. Read and check validity of configuration data")
+    config = GetConf(configpath)
+
+    print ("2. Activate logging scheme as selected in config")
+    config = DefineLogger(config=config, category = "DataProducts", job=os.path.basename(__file__), newname='mm-dp-ace.log', debug=debug)
+
+    name = "{}-ACE-conversion".format(config.get('logname'))
+    currentvaluepath = config.get('currentvaluepath')
+
+    # take localpath from config
+    path = '/srv/archive/external/esa-nasa/ace'
+
+    if starttime:
+        # parse date to stime
+        pass
+    if endtime:
+        # parse date to etime
+        pass
+
+    if not stime:
+        datelist = [datetime.strftime(etime,"%Y%m%d")]
+    else:
+        # time range given... determine list
+        print (" Not implemented so far")
+        pass
+
+    for datum in datelist:
+        print ("Analyzing {}...".format(datum))
+        #--------------------------------------------------------------------
+        # PROCESS 1-MIN DATA
+        #--------------------------------------------------------------------
+        process_ACE(datum, '1m', ['swepam', 'mag'], ['x','y','z','f','t1','t2'], localpath=path, debug=debug)
+
+        #--------------------------------------------------------------------
+        # PROCESS 5-MIN DATA
+        #--------------------------------------------------------------------
+        process_ACE(datum, '5m', ['epam', 'sis'], ['x','y'], skipcompression=True, localpath=path, debug=debug)
+
+
+    statusmsg[name] = 'successfully finished'
+
+    print ("------------------------------------------")
+    print ("  ace conversion finished")
+    print ("------------------------------------------")
+    print ("SUCCESS")
+
+    if not debug:
+        martaslog = ml(logfile=config.get('logfile'),receiver=config.get('notification'))
+        martaslog.telegram['config'] = config.get('notificationconfig')
+        martaslog.msg(statusmsg)
+    else:
+        print ("Debug selected - statusmsg looks like:")
+        print (statusmsg)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ DataImport/dscovr_download_broker.py
@@ -0,0 +1,44 @@
+from magpy.stream import read
+import magpy.mpplot as mp
+from datetime import datetime, timedelta
+
+# Define ranges:
+today = datetime.utcnow()
+yesterday = today - timedelta(days=1)
+starttime = datetime.strftime(yesterday, "%Y-%m-%d") + "T00:00:00"
+endtime =   datetime.strftime(today, "%Y-%m-%d") + "T00:00:00"
+
+# Read data and trim to ranges (for yesterday):
+dscovr_plasma = read("http://services.swpc.noaa.gov/products/solar-wind/plasma-3-day.json")
+dscovr_plasma = dscovr_plasma.trim(starttime=starttime, endtime=endtime)
+dscovr_mag = read("http://services.swpc.noaa.gov/products/solar-wind/mag-3-day.json")
+dscovr_mag = dscovr_mag.trim(starttime=starttime, endtime=endtime)
+
+# Fill out header information:
+dscovr_plasma.header['unit-col-var1'] = "cm^-3"
+dscovr_plasma.header['unit-col-var2'] = "km/s"
+dscovr_plasma.header['unit-col-var3'] = "K"
+dscovr_mag.header['unit-col-x'] = "nT"
+dscovr_mag.header['unit-col-y'] = "nT"
+dscovr_mag.header['unit-col-z'] = "nT"
+dscovr_mag.header['unit-col-f'] = "nT"
+dscovr_mag.header['unit-col-var1'] = "deg"
+dscovr_mag.header['unit-col-var2'] = "deg"
+
+# Write data to archive:
+dscovrpath = "/home/cobs/SPACE/incoming/DSCOVR/collected/"
+dscovr_plasma.write(dscovrpath, format_type="PYCDF", filenamebegins="DSCOVR_plasma_")
+dscovr_mag.write(dscovrpath, format_type="PYCDF", filenamebegins="DSCOVR_mag_")
+
+
+"""
+downloaddict = {"dscovr_plasma" :  {"source"="http://services.swpc.noaa.gov/products/solar-wind/plasma-3-day.json", 
+                                    "savename"="DSCOVR_plasma_", 
+                                    "format"="PYCDF", 
+                                    "header"= {"unit-col-var1"="cm^-3", "unit-col-var2"="km/s", "unit-col-var3"="K"} }
+                "dscovr_mag" :  {"source"="http://services.swpc.noaa.gov/products/solar-wind/plasma-3-day.json", 
+                                    "savename"="DSCOVR_plasma_", 
+                                    "format"="PYCDF", 
+                                    "header"= {"unit-col-var1"="cm^-3", "unit-col-var2"="km/s", "unit-col-var3"="K"} } 
+               }
+"""
