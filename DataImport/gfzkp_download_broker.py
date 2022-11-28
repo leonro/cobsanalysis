9c40224302ec35e46b76b8b031baa0cb4d29a1de
R Bailey <rachel.bailey@oeaw.ac.at>
2022-11-28 12:32:49 -0500

    Added kp-plot download to broker kp script.

--- DataImport/gfzkp_download_broker.py
+++ DataImport/gfzkp_download_broker.py
@@ -12,8 +12,11 @@
 import sys
 from magpy.stream import *
 from magpy.transfer import *
+import urllib.request
+from datetime import datetime
 
 kppath = '/home/cobs/SPACE/incoming/GFZ/kp'
+kpplotpath = '/home/cobs/SPACE/graphs/kp_latest.png'
 
 # READ GFZ KP DATA:
 # -----------------
@@ -21,6 +24,12 @@ kp = read(path_or_url='http://www-app3.gfz-potsdam.de/kp_index/qlyymm.tab')
 
 # Append that data to local list:
 kp.write(kppath,filenamebegins='gfzkp',format_type='PYCDF',dateformat='%Y%m',coverage='month')
+
+# DOWNLOAD CURRENT KP PLOT:
+# -------------------------
+today = datetime.utcnow().strftime("%Y-%m-%d")
+url_plot = "https://kp.gfz-potsdam.de/fileadmin/kp-archiv/png/{}.png".format(today)
+urllib.request.urlretrieve(url_plot, kpplotpath)
 
 print ("Kp File written successfully.")
 print ("-----------------------------")
